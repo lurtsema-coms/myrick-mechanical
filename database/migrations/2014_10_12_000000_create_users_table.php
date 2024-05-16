8b5cfd6752d0643c8986a77779745aca75163c65
GabQuing <gab.quing29@gmail.com>
2024-05-17 07:57:59 +0800

    edit user migration and remove register blade

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->mediumInteger('created_by')->unsigned()->nullable();
+            $table->mediumInteger('updated_by')->unsigned()->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
