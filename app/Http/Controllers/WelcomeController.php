5dcda37509613c71a1861c3da0c4cc0b538cc5f2
Fil <filongunio@gmail.com>
2024-05-25 02:28:43 +0800

    hide expired and deleted ads

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,9 +10,12 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $data=[];
+        $data = [];
         $data['Ads1'] = Ad::where('ad_placement', 'Placement 1')
+            ->whereNull('deleted_at')
+            ->where('from_date', '<=', date('Y-m-d'))
+            ->where('to_date', '>=', date('Y-m-d'))
             ->get();
-        return view('welcome',$data);
+        return view('welcome', $data);
     }
 }
